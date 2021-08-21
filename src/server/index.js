fe1fdc014a61005d57ef4d470ed493a1eb77deb6
Tria <tria22@users.noreply.github.com>
2021-08-21 21:10:05 +0200

    add Token
    add the authentication token.

--- src/server/index.js
+++ src/server/index.js
@@ -4,17 +4,21 @@ const express = require('express');
 //require('dotenv').config();
 const jsforce = require('jsforce');
 
-const { SF_USERNAME, SF_PASSWORD } = process.env;
-if (!(SF_USERNAME && SF_PASSWORD)) {
+if(process.env.NODE_ENV !== 'production'){
+    require('dotenv').config()
+}
+
+const { SF_USERNAME, SF_PASSWORD, SF_TOKEN, SF_LOGIN_URL } = process.env;
+if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
     console.error(
         'Cannot start app: missing mandatory configuration. Check your .env file.'
     );
     process.exit(-1);
 }
 const conn = new jsforce.Connection({
-    loginUrl: 'https://login.salesforce.com'
+    loginUrl: SF_LOGIN_URL
 });
-conn.login(SF_USERNAME, SF_PASSWORD, err => {
+conn.login(SF_USERNAME, SF_PASSWORD + SF_TOKEN, err => {
     if (err) {
         console.error(err);
         process.exit(-1);
